290e9235462464f304f6cf8d0f6c864d40ec1e09
run4w4y <add4che@gmail.com>
2019-05-04 13:13:39 +0300

    update profile.py

--- profile.py
+++ profile.py
@@ -8,6 +8,7 @@ class Profile:
         self.data = {}
 
         for attr, value in self.raw_data.items():
+            attr = attr.string
             if 'Имя' in attr:
                 self.name = value.b.string
                 self.data['name'] = self.name
@@ -15,14 +16,32 @@ class Profile:
                 self.login = value.b.string
                 self.data['login'] = self.login
             elif 'Школа' in attr:
-                self.school = value
+                self.school = value.string
                 self.data['school'] = self.school
             elif 'Должность' in attr:
-                self.position = value
+                self.position = value.string
                 self.data['position'] = self.position
+            elif 'рождения' in attr:
+                self.birthday = value.string
+                self.data['birthday'] = self.birthday
+            elif 'Пол' in attr:
+                self.gender = value.string
+                self.data['gender'] = self.gender
+            elif 'Сертификата' in attr:
+                self.cert = value.b.string
+                self.data['cert'] = self.cert
 
 
-    def __init__(self, session):
+    def __init__(self, session, data=None):
+        if data is not None:
+            self.data = data
+            self.raw_data = None
+
+            for attr, value in data.items():
+                exec('self.{} = {}'.format(attr, value))
+
+            return
+
         response = session.get(index_url)
 
         if 'Войти через ЕСИА' in response.text:
@@ -38,3 +57,7 @@ class Profile:
             self.raw_data[cols[0]] = cols[1]
         
         self.parse_raw()
+
+
+    def to_str(self):
+        return str(self.data)
