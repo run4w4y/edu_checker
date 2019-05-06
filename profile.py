940a787a0ef6c5c2f9731a96726e0ca21f9f2c4a
run4w4y <add4che@gmail.com>
2019-05-06 15:40:46 +0300

    fix typo

--- profile.py
+++ profile.py
@@ -13,6 +13,7 @@ except ImportError:
 
 def check_login(f):
     def wrap(self, *args, **kwargs):
+        print(self.session.get(index_url, proxies=self.proxy).text, self.credentials['main_login'])
         if self.credentials['main_login'] not in self.session.get(index_url, proxies=self.proxy).text:
             self.logout()
             self.login()
